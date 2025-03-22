5cf200da0a31ca52b535e6697b63fb9e6d4072de
Tereza Koriťáková <koritter@fel.cvut.cz>
2025-03-22 15:35:40 +0100

    Activity creation test

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 
 function App() {
 
+  const act1 = new Activity('Zahájení', 2.0, 1, 100, 100);
+  const act2 = new Activity('Kontrola', 3.0, 2, 300, 100);
 
   return (
-    
-      
-      <p >
-        BPMN simulátor
-      </p>
-   
+    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+      <ActivityView activity={act1} heat={0.0} />
+      <ActivityView activity={act2} heat={0.6} />
+    </div>
   );
+
+
 }
 
 export default App;
