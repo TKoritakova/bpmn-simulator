6d044543414c8c08e01c3cdc4f06d2cab9befd25
Tereza Koriťáková <koritter@fel.cvut.cz>
2025-05-04 20:00:08 +0200

    lesson 3 diagram

--- src/front/slides/Chap3Slide4.jsx
+++ src/front/slides/Chap3Slide4.jsx
@@ -1,18 +1,43 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, useRef } from "react";
+import BpmnViewer from 'bpmn-js';
 
 export default function Chap3Slide4({ setSlideFinished }) {
 
+  const containerWorkshopRef = useRef(null);
+  const viewerWorkshopRef = useRef(null);
 
+  useEffect(() => {
+    fetch('dilna-ver1.bpmn') 
+    .then(res => res.text())
+    .then(text => {
+      
+      if (!viewerWorkshopRef.current) {
+        viewerWorkshopRef.current = new BpmnViewer({ container: containerWorkshopRef.current });
+      }
+      viewerWorkshopRef.current.importXML(text).then(async () => {
+        viewerWorkshopRef.current.get('canvas').zoom('fit-viewport');                
+                  
+      }).catch(err => {
+          console.error('Chyba při načítání BPMN XML:', err);
+      });
+    })
+    .catch(err => {
+    console.error('Chyba při načítání BPMN souboru:', err);
+    });
 
-
+  }, []);
 
     return <div className="slide">
       <div className='slide-h1-wrapper'><h1>První simulace</h1></div>
       <div className='slide-content-wrapper'>
-        {/* lit 4. - str. 303 */}
+       
 
-
+      <div className='bpmn-diagram-wrapper'>
+        <div
+        ref={containerWorkshopRef}
+        className='bpmn-diagram-supermarketver2'
+        />
+      </div>
           
 
 
